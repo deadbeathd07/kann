3f9c958438f50ce5c9f039558f3629ba13b39f29
deadbeathd07 <deadbeat.hd.07@gmail.com>
2023-10-05 17:31:48 +0300

    создан компонент навигации, в него был импортирован компонент списка

---
+++ src/components/KannNavigation.vue
@@ -0,0 +1,17 @@
+<template lang="pug">
+nav.nav
+  KannList(:list='props.menu')
+</template>
+
+<script setup>
+import KannList from './KannList.vue';
+
+const props = defineProps({
+	menu: {
+		type: Array,
+		required: true,
+	},
+});
+</script>
+
+<style lang="scss"></style>
