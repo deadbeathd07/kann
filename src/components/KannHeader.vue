5a0c2a71a0e2ebe98efa24a5270d7a482de4ac36
deadbeathd07 <deadbeat.hd.07@gmail.com>
2023-10-05 17:34:36 +0300

    в коспонент хэдэра были импортированы все необходимые компоненты, также были внесены начальные данные для них

--- src/components/KannHeader.vue
+++ src/components/KannHeader.vue
@@ -1,10 +1,32 @@
 <template lang="pug">
 header.header
   KannLogo(alt='Organisation\'s logo')
+  KannNavigation(:menu='menu')
+  KannLangSwitcher(:lang-arr='languages')
+  KannSearch
+  a.link(href="#") points de vente
+  KannCart
 </template>
 
 <script setup>
 import KannLogo from './KannLogo.vue';
+import KannNavigation from './KannNavigation.vue';
+import KannLangSwitcher from './KannLangSwitcher.vue';
+import KannSearch from './KannSearch.vue';
+import KannCart from './KannCart.vue';
+
+const menu = [
+	{ name: 'Shop', path: '#' },
+	{ name: 'Projets', path: '#' },
+	{ name: 'Journal', path: '#' },
+	{ name: 'à propos', path: '#' },
+];
+
+const languages = ['fr', 'en'];
 </script>
 
-<style lang="scss"></style>
+<style lang="scss" scoped>
+.header {
+	display: flex;
+}
+</style>
