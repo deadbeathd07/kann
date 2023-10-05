274ac347e7b70754a1fff25050c92e233daa8d4a
deadbeathd07 <deadbeat.hd.07@gmail.com>
2023-10-05 17:30:10 +0300

    создан компонент переключения языка

---
+++ src/components/KannLangSwitcher.vue
@@ -0,0 +1,15 @@
+<template lang="pug">
+div.lang-switcher
+  button(v-for='(button, i) in langArr', :key='i', type='button') {{ button }}
+</template>
+
+<script setup>
+const props = defineProps({
+	langArr: {
+		type: Array,
+		required: true,
+	},
+});
+</script>
+
+<style lang="scss"></style>
