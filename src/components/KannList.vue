5c1d8948aa6e57333c80b099bf7de79e1e5e7c90
deadbeathd07 <deadbeat.hd.07@gmail.com>
2023-10-05 17:30:34 +0300

    создан компонент списка

---
+++ src/components/KannList.vue
@@ -0,0 +1,16 @@
+<template lang="pug">
+ul.list
+  li.list__item(v-for="(item, i) in props.list", :key="i") 
+    a.link(:href="item.path") {{ item.name }}
+</template>
+
+<script setup>
+const props = defineProps({
+	list: {
+		type: Array,
+		required: true,
+	},
+});
+</script>
+
+<style lang="scss"></style>
