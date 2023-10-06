ee2f321b391961df1da2ad547db692adfde84103
deadbeathd07 <deadbeat.hd.07@gmail.com>
2023-10-06 22:51:32 +0300

    импортирован футер и внесен в разметку

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,14 @@
 <script setup>
 import KannHeader from './components/KannHeader.vue';
+import KannFooter from './components/KannFooter.vue';
 </script>
 
 <template lang="pug">
 div
   KannHeader
+  main
+    section
+  KannFooter
 </template>
 
 <style lang="scss"></style>
