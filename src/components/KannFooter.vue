edea4f70fa37d0f3a2bd5ca10873d8493acda52e
deadbeathd07 <deadbeat.hd.07@gmail.com>
2023-10-06 23:11:05 +0300

    добавлены классы для элементов, модификаторов элементов и блоков

--- src/components/KannFooter.vue
+++ src/components/KannFooter.vue
@@ -2,11 +2,11 @@
 footer.footer.footer--top-border
   section.footer__menu-section
     KannListSection(v-for='(listSection, i) in listSections', :key='i', :listSection='listSection' classList="footer__list fs-15")
-  section.footer__copywriting
+  section.footer__copywriting.footer__copywriting--light.footer__copywriting--lowercase.fs-15
     p © Kann Design 2021
-    button.button.button--basic-button(type="button") Gestion des cookies
+    button.button.button--basic-button.footer__button.footer__button--light.footer__button--hover.fs-15(type="button") Gestion des cookies
     p Website by
-    a.link.link--basic-link(href='#') Studio Krabb &#38; Léa Tortay
+    a.link.link--basic-link.footer__link.footer__link--light.footer__link--hover(href='#') Studio Krabb &#38; Léa Tortay
 </template>
 
 <script setup>
@@ -60,10 +60,33 @@ const listSections = [
 		width: 80%;
 	}
 	&__copywriting {
-		padding: 0 17px;
+		display: flex;
+		align-items: center;
+		padding: 17px 0;
+		&--light {
+			color: lighten($color: #1a1818, $amount: 60);
+		}
+		&--lowercase {
+			text-transform: lowercase;
+		}
 	}
 	&__list > * + * {
 		margin-top: 8px;
+	}
+	&__button.button {
+		margin-right: auto;
+		margin-left: 20px;
+	}
+	&__link.link {
+		margin-left: 5px;
+	}
+	&__button--light,
+	&__link--light {
+		color: lighten($color: #1a1818, $amount: 60);
+	}
+	&__button--hover:hover,
+	&__link--hover:hover {
+		color: lighten($color: #1a1818, $amount: 40);
 	}
 	&--top-border {
 		border-top: 2px solid #1a1818;
