c2f268ce9309e0917f5094adcbb7086d55a9949c
deadbeathd07 <deadbeat.hd.07@gmail.com>
2023-10-10 15:00:43 +0300

    были добавлены стили элементов блока секции, добавлены модификаторы, также данные классы были внесены в верстку, добавлен атомарный класс для автоматического внешнего отступа сверху

--- src/App.vue
+++ src/App.vue
@@ -7,12 +7,14 @@ import KannFooter from './components/KannFooter.vue';
 div.layout__wrapper
   KannHeader(class="layout__header")
   main.layout__content
-    section.layout__section--position-top-70.section.section--bg-img.section--min-height-80
-      h1.title.title--basic-color.fs-60.ls-12 Galta collection by SCMP Design Office
-    section.section
-      h2.title.title--basic-color.fs-60.ls-12 Dans les coulisses de la fabrication Kann
-      img(src="./assets/images/img-1.jpg")
-      a.link.link--basic-link.link--hover.fs-14(href="#") Lire l'article
+    section.layout__section--position-top-70.section.section--bg-img.section--min-height-70.section--max-height-90
+      h1.title.title--basic-color.section__grid-element.section__grid-element--rows-full.section__grid-element--column-start-4.section__flex-element.section__flex-element--ai-center.fs-60.ls-12 Galta collection by SCMP Design Office
+    section.section.section--min-height-70.section--max-height-90
+      div.section__grid-element.section__grid-element--rows-full.section__grid-element--column-start-4.section__flex-element.section__flex-element--vertical
+        h2title.title--basic-color.fs-60.ls-12.mt-auto Dans les coulisses de la fabrication Kann
+        a.link.link--basic-link.link--hover.fs-14.mt-auto(href="#") Lire l'article
+      figure.section__grid-element.section__grid-element--rows-full.section__grid-element--column-end-7
+        img.section__img(src="./assets/images/img-1.jpg")
     section.section
       img(src="./assets/images/img-2.jpg")
       img(src="./assets/images/img-3.jpg")
