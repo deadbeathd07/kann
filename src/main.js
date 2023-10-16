ac02a9f7fbdb177af3d01753a31f5130a5931f09
deadbeathd07 <deadbeat.hd.07@gmail.com>
2023-10-16 15:45:24 +0300

    был импортирован компонент иконок fonaesome, была подключена библиотека иконок и импортирована иконка меню

--- src/main.js
+++ src/main.js
@@ -1,5 +1,10 @@
 import { createApp } from 'vue';
 import './assets/styles/index.scss';
 import App from './App.vue';
+import { library } from '@fortawesome/fontawesome-svg-core';
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome';
+import { faBars } from '@fortawesome/free-solid-svg-icons';
 
-createApp(App).mount('#app');
+library.add(faBars);
+
+createApp(App).component('font-awesome-icon', FontAwesomeIcon).mount('#app');
