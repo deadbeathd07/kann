1d1412a5b4cefb6fe1639374154c922252771f36
deadbeathd07 <deadbeat.hd.07@gmail.com>
2023-10-16 19:55:48 +0300

    создан и сформирован компонент формы

---
+++ src/components/KannForm.vue
@@ -0,0 +1,19 @@
+<template lang="pug">
+form.form
+  h3.form__title(v-if="formContent.title") {{ formContent.title }}
+  input.form__email(type="email", name="form__email")
+  KannButton(type="submit", :text="formContent.button_text")
+</template>
+
+<script setup>
+import KannButton from './KannButton.vue';
+
+const formContent = defineProps({
+	formContent: {
+		type: Object,
+		required: true,
+	},
+});
+</script>
+
+<style lang="scss"></style>
